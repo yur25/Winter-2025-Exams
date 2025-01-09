3f8a7c4b6f3b229389613104ad3a1df66b5ade17
Yurii <yurachoruk123@gmail.com>
2025-01-09 22:22:52 +0200

    declared variables with const, removed unnecessary 'else' statements

--- Tasks/between.js
+++ Tasks/between.js
@@ -3,20 +3,15 @@
 
 'use strict'
 
-getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p)
+const getvaluebetween = (str, p, s) => {
+  const i = str.indexOf(p)
   if (i === -1) return ''
-  else {
-    k = i + p.length
-    str = str.substring(k)
-    if (s) {
-      i = str.indexOf(s)
-      if (i === -1) {
-        return ''
-      } else {
-        str = str.substring(0, i)
-      }
-    }
+  const k = i + p.length
+  const str = str.substring(k)
+  if (s) {
+    i = str.indexOf(s)
+    if (i === -1) return ''
+    str = str.substring(0, i)
   }
   return str
 }
