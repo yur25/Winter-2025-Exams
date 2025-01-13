2e0b97c7de5dc092e964ec9cdbf1db9749e7a4d4
Yurii <yurachoruk123@gmail.com>
2025-01-13 16:19:26 +0200

    removed unused parameters, used const and 'use strict'

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,9 +1,11 @@
 // Refactor following solution
 // Reverse an array, you can't use .reverse()
 
-invert = (A, i, j, k) => {
-  result = []
-  for (const item of A){
+'use strict'
+
+const invert = (array) => {
+  const result = []
+  for (const item of array){
     result.unshift(item)
   }
   return result
