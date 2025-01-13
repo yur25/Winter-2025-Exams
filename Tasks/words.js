d03ddbaf8c0ad5ed493d12598bda841a794b4b83
Yurii <yurachoruk123@gmail.com>
2025-01-13 21:25:52 +0200

    used a '.split' function instead of a loop, removed unnecessary code

--- Tasks/words.js
+++ Tasks/words.js
@@ -4,40 +4,8 @@
 'use strict'
 
 const words = (string) => {
-  const wordNumber = 0
-  flag = false
-  for (c of string) {
-    if (!flag) {
-      if (c === ' ') {
-        if (flag === true) {
-          flag = false;
-        } else {
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
-        wordNumber++;
-      }
-    } else {
-      if (c === ' ') {
-        if (flag === true) {
-          flag = false;
-        } else {
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
-      }
-    }
-  }
+  const wordList = string.split(' ')
+  const wordNumber = wordList.length
   return wordNumber
 }
 
